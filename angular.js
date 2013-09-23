1b67fbd9687b9cb67a9bbe4a808d3c0e1ea376aa
Murat Çorlu <muratcorlu@gmail.com>
2013-09-23 16:43:51 +0300

    DELETE requestlerinde content-type gonderme sorunsali

--- angular.js
+++ angular.js
@@ -6727,7 +6727,7 @@ function createHttpBackend($browser, XHR, $browserDefer, callbacks, rawDocument,
         xhr.responseType = responseType;
       }
 
-      xhr.send(post || '');
+      xhr.send(post || null);
     }
 
     if (timeout > 0) {
